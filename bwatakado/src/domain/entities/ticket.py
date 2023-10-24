03487400cbb59d15b64177e35d575cd909ccc504
Christophe 'DrakeXorn' Bernard <drakexorn@gmail.com>
2023-10-24 17:02:17 +0200

    refactor(Tickets): Resolve comments

--- bwatakado/src/domain/entities/ticket.py
+++ bwatakado/src/domain/entities/ticket.py
@@ -8,6 +8,6 @@ from dataclasses import dataclass, field
 class Ticket:
     code: str = field(default_factory=lambda: str(uuid.uuid4()), repr=True, init=False)
     is_winning: bool = field(repr=True, init=True)
-    created_at: datetime = field(default_factory=datetime.now, repr=True)
+    created_at: datetime = field(default_factory=datetime.now, repr=True, init=False)
     is_printed: bool = field(default=False, repr=True, init=False)
     has_been_claimed: bool = field(default=False, repr=True, init=False)
--- bwatakado/tests/domain/entities/test_ticket.py
+++ bwatakado/tests/domain/entities/test_ticket.py
@@ -1,10 +1,9 @@
-import unittest
+from unittest import mock, TestCase
+
 from bwatakado.src.domain.entities.ticket import Ticket
 
-from unittest import mock
 
-
-class TicketTest(unittest.TestCase):
+class TicketTest(TestCase):
     @mock.patch('uuid.uuid4', return_value='1')
     def test_winning_ticket_init(self, mock_uuid):
         ticket = Ticket(True)
@@ -15,7 +14,6 @@ class TicketTest(unittest.TestCase):
             f"Ticket code is not correct, should be {mock_uuid.return_value}, got {ticket.code}"
         )
         self.assertTrue(ticket.is_winning)
-        self.assertFalse(ticket.is_printed)
 
     @mock.patch('uuid.uuid4', return_value='1')
     def test_non_winning_ticket_init(self, mock_uuid):
